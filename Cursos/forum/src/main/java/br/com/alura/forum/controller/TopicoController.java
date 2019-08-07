21b764c72ebbe7897e9711079c4f5c540c899489
Igor Ferreira de Almeida <igor.rigel@gmail.com>
2019-08-07 18:27:29 -0300

    Spring Boot Parte 2: Segurança da API, Cache e Monitoramento

--- Cursos/forum/src/main/java/br/com/alura/forum/controller/TopicoController.java
+++ Cursos/forum/src/main/java/br/com/alura/forum/controller/TopicoController.java
@@ -1,14 +1,16 @@
 package br.com.alura.forum.controller;
 
 import java.net.URI;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
 import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +19,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -39,14 +42,17 @@ public class TopicoController {
 	private CursoRepository cursoRepository;
 
 	@GetMapping
-	public List<TopicoDTO> list(String nomeCurso) {
-		List<Topico> topicos = new ArrayList<>();
+	public Page<TopicoDTO> listar(@RequestParam(required = false) String nomeCurso, @RequestParam int pagina, @RequestParam int quantidade,  @RequestParam String ordenacao) {
+		
+		Pageable paginacao = PageRequest.of(pagina, quantidade, Direction.DESC, ordenacao);
+		
 		if(nomeCurso == null) {
-			topicos = topicoRepository.findAll();
+			Page<Topico> topicos = topicoRepository.findAll(paginacao);
+			return TopicoDTO.converter(topicos);
 		} else {
-			topicos = topicoRepository.findByCursoNome(nomeCurso);
+			Page<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso, paginacao);
+			return TopicoDTO.converter(topicos);
 		}
-		return TopicoDTO.converter(topicos);
 	}
 	
 	@PostMapping
--- Cursos/forum/src/main/java/br/com/alura/forum/dto/TopicoDTO.java
+++ Cursos/forum/src/main/java/br/com/alura/forum/dto/TopicoDTO.java
@@ -4,6 +4,8 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.data.domain.Page;
+
 import br.com.alura.forum.model.Topico;
 import lombok.Getter;
 
@@ -28,8 +30,8 @@ public class TopicoDTO {
 		dataCriacao = topico.getDataCriacao();
 	}
 	
-	public static List<TopicoDTO> converter(List<Topico> topicos) {
-		return topicos.stream().map(TopicoDTO::new).collect(Collectors.toList());
+	public static Page<TopicoDTO> converter(Page<Topico> topicos) {
+		return topicos.map(TopicoDTO::new);
 	}
 	
 	
