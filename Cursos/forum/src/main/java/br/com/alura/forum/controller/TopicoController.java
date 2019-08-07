b83e9a5413f9d59927da0b728f2448a6057fa563
Igor Ferreira de Almeida <igor.rigel@gmail.com>
2019-08-07 18:37:01 -0300

    Spring Boot Parte 2: Segurança da API, Cache e Monitoramento

--- Cursos/forum/src/main/java/br/com/alura/forum/controller/TopicoController.java
+++ Cursos/forum/src/main/java/br/com/alura/forum/controller/TopicoController.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -42,9 +43,7 @@ public class TopicoController {
 	private CursoRepository cursoRepository;
 
 	@GetMapping
-	public Page<TopicoDTO> listar(@RequestParam(required = false) String nomeCurso, @RequestParam int pagina, @RequestParam int quantidade,  @RequestParam String ordenacao) {
-		
-		Pageable paginacao = PageRequest.of(pagina, quantidade, Direction.DESC, ordenacao);
+	public Page<TopicoDTO> listar(@RequestParam(required = false) String nomeCurso, @PageableDefault(sort = "id", direction = Direction.ASC, page = 0, size = 10) Pageable paginacao) {
 		
 		if(nomeCurso == null) {
 			Page<Topico> topicos = topicoRepository.findAll(paginacao);
